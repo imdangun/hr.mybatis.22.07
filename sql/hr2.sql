32624734dacb182f900358cb4f0639a384861886
남희석 <nam@my.com>
2022-11-01 18:27:08 +0900

    a

--- sql/hr2.sql
+++ sql/hr2.sql
@@ -7,7 +7,7 @@ grant connect, resource to hr2;
 
 create table hr2.laborers (
 laborer_id number(3) constraint laborer_id_pk primary key,
-name varchar2(12) constraint laborer_name_nn not null,
+laborer_name varchar2(12) constraint laborer_name_nn not null,
 hire_date date constraint laborer_hiredate_nn not null);
 
 create sequence hr2.laborer_id_seq;
